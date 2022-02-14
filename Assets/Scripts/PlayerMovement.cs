0cac97d258fde38c6ae173201d59f3c5d101c129
nicholasarinduque <89489431+nicholasarinduque@users.noreply.github.com>
2022-02-14 15:30:57 -0800

    home

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,59 +4,39 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Animator animator;
-    private Animator m_Animator;
-    private Rigidbody rb;
-    private Vector3 moveDirection;
-    private Quaternion rotation;
-    private bool isWalking;
-    
+    public float turnSpeed = 20f;
 
-    [SerializeField] private float turnSpeed = 20f;
+    Animator m_Animator;
+    Rigidbody m_Rigidbody;
+    Vector3 m_Movement;
+    Quaternion m_Rotation = Quaternion.identity;
 
-    // Start is called before the first frame update
     void Start()
     {
         m_Animator = GetComponent<Animator>();
-        rb = GetComponent<Rigidbody>();
-        rotation = Quaternion.identity;
+        m_Rigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame 
     void FixedUpdate()
     {
-        // Get user input
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        // Use user input to set direction player will move
-        moveDirection.Set(horizontal, 0f, vertical);
-        moveDirection.Normalize();
-
-        // Set animator to walking or idle depending on user input
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize();
 
         bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
         bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
         bool isWalking = hasHorizontalInput || hasVerticalInput;
         m_Animator.SetBool("IsWalking", isWalking);
-        // Assign roation towards move direction 
-        Vector3 desiredDirection = Vector3.RotateTowards(transform.forward, moveDirection,
-            turnSpeed * Time.deltaTime, 0f);
-        rotation = Quaternion.LookRotation(desiredDirection);
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation(desiredForward);
     }
 
-    // Animator event
-    private void OnAnimatorMove()
+    void OnAnimatorMove()
     {
-        rb.MovePosition(rb.position + moveDirection * animator.deltaPosition.magnitude);
-        rb.MoveRotation(rotation);
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
+        m_Rigidbody.MoveRotation(m_Rotation);
     }
-
-
-
-
-
-
-
-
 }
